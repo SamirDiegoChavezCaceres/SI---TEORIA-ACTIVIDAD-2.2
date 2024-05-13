0babb3d023d7980fddc8f5bdcef5532c180707af
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2024-05-12 23:10:02 -0500

    Update atbash.py

--- algorithms/atbash.py
+++ algorithms/atbash.py
@@ -17,4 +17,19 @@ class AtbashAlgorithm(Algorithm):
     def execute(self, mensaje):
         """ Método para cifrar un mensaje
         """
+        mensaje = self.clean_text(mensaje)
         return mensaje[::-1]
+
+    def clean_text(self, text):
+        """ Método para limpiar un texto
+        """
+        tildes = {'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 'ü': 'u'}
+        resultado = ''
+
+        # Eliminar signos de puntuación y convertir a minúsculas
+        clean_text = re.sub(r'[^\w\s]', '', text.lower())
+        # Reemplazar letras con tildes por letras básicas
+        for c in clean_text:
+            resultado += tildes.get(c, c)
+        return resultado.strip().replace(" ", "")
+
